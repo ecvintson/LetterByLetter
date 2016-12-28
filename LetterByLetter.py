b26f8a9bd60499299bf112c665c2610f90f124ea
ecvintson <ecvintson@gmail.com>
2016-12-28 15:26:21 -0500

    v0.2
    improved printing
    comments

--- LetterByLetter.py
+++ LetterByLetter.py
@@ -1,4 +1,7 @@
 # Objective: change a word to another word, letter by letter
+# add checks to make sure words are same length
+# improve, so words don't need to be same length
+# improve to change entire sentences into other sentences
 
 print "Enter first word:"
 at = raw_input()
@@ -11,10 +14,12 @@ b = list(bt)
 
 c = len(a)
 i = 0
-
+x = ""
+print "________________"
 while (i < c):
-	print a
+	print x.join(a)
 	a[i] = b[i]
 	i = i + 1
 
-print a
+
+print x.join(a)
