52b5ce67dee88d03b8f8342b3169dbbb57c6f444
ecvintson <ecvintson@gmail.com>
2016-12-29 16:44:55 -0500

    v0.3
    add checks to compare lengths, add blank space to shorter input
    can now change entire sentences into other given input
    will always change the shorter input into the longer input

--- LetterByLetter.py
+++ LetterByLetter.py
@@ -1,7 +1,4 @@
 # Objective: change a word to another word, letter by letter
-# add checks to make sure words are same length
-# improve, so words don't need to be same length
-# improve to change entire sentences into other sentences
 
 print "Enter first word:"
 at = raw_input()
@@ -13,13 +10,38 @@ a = list(at)
 b = list(bt)
 
 c = len(a)
-i = 0
-x = ""
-print "________________"
-while (i < c):
-	print x.join(a)
-	a[i] = b[i]
-	i = i + 1
+d = len(b)
+e = 0
+
+def main(longer, shorter, count):
+	i = 0
+	x = ""
+	print "________________"
+	while (i < count):
+		print x.join(shorter)
+		shorter[i] = longer[i]
+		i = i + 1
 
 
-print x.join(a)
+	print x.join(shorter)
+
+if (c > d):
+	z = (c - d)
+	while z > 0:
+		b.append(" ")
+		z = z-1
+	main(a, b, c)
+
+
+elif (d > c):
+	z = (d - c)
+	while z > 0:
+		a.append(" ")
+		z = z - 1
+	main(b, a, d)
+else:
+	main(a, b, c)
+
+
+
+
